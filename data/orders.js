7a15b2afcf036e925cbad568d56611ab084ecfac
UditPatankar <augustnine64@gamil.com>
2025-08-15 21:15:39 +0530

    Make tracking page interactive

--- data/orders.js
+++ data/orders.js
@@ -9,4 +9,16 @@ function saveToStorage() {
   localStorage.setItem('orders', JSON.stringify(orders));
 }
 
-console.log(orders);
+export function getOrder(orderId) {
+  let matchingOrder;
+
+  orders.forEach((order) => {
+    if(order.id === orderId) {
+      matchingOrder = order;
+    }
+  });
+
+  return matchingOrder;
+}
+
+console.log(getOrder('5103d5ea-199b-4810-8d4a-c1aa5f605269'));
